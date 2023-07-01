c61a796639cd9e806ad18150e0f86419755d148d
Lemar Levy <lemarlevy@gmail.com>
2023-07-01 16:30:47 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
+import './index.css';
+
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  return (
-    <>
+  function handleClick() {
+    // some logic here
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
   
-    </>
-  )
+return (
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+     {featPupId !==null&& <p>{featPupId}</p>}
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
-
 export default App
+
+
